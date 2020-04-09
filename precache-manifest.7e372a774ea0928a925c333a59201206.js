7dcd0e6b1129f1e6839de80c14ff4170f0943d3c
kapil jhajhria <jhajhria44@gmail.com>
2020-04-09 13:16:08 +0530

    movie project init

---
+++ precache-manifest.7e372a774ea0928a925c333a59201206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9600a4c30013a9ec1dbc159d4a52e39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c77000628e9e55cc2ab6",
+    "url": "/static/css/main.076704d8.chunk.css"
+  },
+  {
+    "revision": "2a02ce2ae7a5f6b0deb4",
+    "url": "/static/js/2.beae8ea6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.beae8ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77000628e9e55cc2ab6",
+    "url": "/static/js/main.fdc4d8ec.chunk.js"
+  },
+  {
+    "revision": "90862c121b685a23a76c",
+    "url": "/static/js/runtime-main.10e7cb3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
